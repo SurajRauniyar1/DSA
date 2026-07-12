5e791e3259bde6e6a600f6bb46098504e2fc2e1f
SurajRauniyar1 <rauniyarsuraj667@gmail.com>
2026-07-12 08:49:40 +0530

    update

---
+++ ref.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 10;
+    int* ptr = &x;
+
+    cout<<x<<endl;
+    cout<<&x<<endl;
+    cout<<ptr<<endl;
+    cout<<*ptr<<endl;
+    *ptr =50;
+    cout<<x;
+}
---
+++ ref1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a =5;
+    int b =10;
+    int* p = &a;
+    p = &b;
+    *p = 20;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    return 0;
+}
